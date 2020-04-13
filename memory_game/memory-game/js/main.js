9b5ee09ca8174a017416509f193f66ace5565ba9
Andrew Taghizadeh <AndrewTaghi@macbook-air.attlocal.net>
2020-04-13 15:22:24 -0500

    created game logic

---
+++ memory_game/memory-game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+console.log("User Flipped " + cardFour);
+console.log("User Flipped " + cardOne);
