38fe3d684b4aedec22a7227c6369cf36d4da9f87
Andrew Taghizadeh <AndrewTaghi@macbook-air.attlocal.net>
2020-04-15 13:20:02 -0500

    Completed Control Flow Section

--- memory_game/memory-game/js/main.js
+++ memory_game/memory-game/js/main.js
@@ -3,8 +3,16 @@ let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
 let cardOne = cards[0];
+let cardTwo = cards[1];
 
 cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cards[2]);
+console.log("User Flipped " + cardTwo);
+
+if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a Match!");
+} else {
+	alert("Sorry Try Again!");
+}
